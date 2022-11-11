cce717e88f60adcdd5614305571522998e17562e
MariaEmiliaWalter <79611670+MariaEmiliaWalter@users.noreply.github.com>
2022-11-11 10:36:34 -0300

    tagController

--- src/main/java/com/mindhub/overflow/controllers/TagController.java
+++ src/main/java/com/mindhub/overflow/controllers/TagController.java
@@ -1,15 +1,19 @@
 package com.mindhub.overflow.controllers;
 
+import com.mindhub.overflow.dtos.QuestionDTO;
 import com.mindhub.overflow.dtos.TagDTO;
 import com.mindhub.overflow.models.Tag;
 import com.mindhub.overflow.services.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/tag")
@@ -21,6 +25,11 @@ public class TagController {
     @GetMapping(value = "")
     public Set<TagDTO> getTags (){
         return tagService.getTags();
+    }
+
+    @GetMapping(value = "/{subject}")
+    public List<QuestionDTO> getQuestionByTagSubject(@PathVariable String subject){
+        return tagService.getQuestionByTagSubject(subject);
     }
 
 }
