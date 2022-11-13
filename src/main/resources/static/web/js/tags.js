776d4e6d06b63296eca7c537ded6517f607d19b5
MariaEmiliaWalter <79611670+MariaEmiliaWalter@users.noreply.github.com>
2022-11-13 01:49:21 -0300

    correccion html, css, js

--- src/main/resources/static/web/js/tags.js
+++ src/main/resources/static/web/js/tags.js
@@ -15,11 +15,27 @@ var app = new Vue({
         axios.get("/api/current").then((response) => {
              //get user info
             this.user = response.data
+            axios.get("/api/tag").then((response) => {
+                this.tags = response.data
+                }).catch((error)=>{
+                    // handle error
+                    this.errorMsg = "Error getting data";
+                    this.errorToats.show();
+                })
             }).catch((error)=>{
                   // handle error
                   this.errorMsg = "Error getting data";
                   this.errorToats.show();
               })
         },
+        tagsFormat: function(tags){
+            tags =  tags.map( tag => tag.subject);
+            //return tags.join(", ");
+            return tags;
+        },
+
+       redirectTag: function(tag){
+            window.location.href="http://localhost:8080/web/tag.html?subject="+tag;
+       }
   }
   })
